ec3b4fde0ef5ff08329035411f87b12c3c490439
Max Kapur <git@maxkapur.com>
2025-09-26 15:28:18 -0500

    Basic secant algorithm

---
+++ test/test_water_filling.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pytest
+
+import water_filling
+
+
+@pytest.mark.parametrize(
+    "heights,level,volume",
+    [
+        ([1, 2, 3, 4], 0.5, 0.0),
+        ([1, 2, 3, 4], 1.0, 0.0),
+        ([1, 2, 3, 4], 1.5, 0.5),
+        ([1, 2, 3, 4], 2.5, 2.0),
+        ([1, 2, 3, 4], 4.0, 6.0),
+        ([1, 2, 3, 4], 10.0, 30.0),
+        ([-1, 2, -3, -4], -4.5, 0.0),
+        ([-1, 2, -3, -4], -4.0, 0.0),
+        ([-1, 2, -3, -4], -3.5, 0.5),
+        ([-1, 2, -3, -4], 0.0, 8.0),
+        ([-1, 2, -3, -4], 1.5, 12.5),
+        ([-1, 2, -3, -4], 2.0, 14.0),
+        ([-1, 2, -3, -4], 3.0, 18.0),
+    ],
+)
+def test_volume(heights, level, volume):
+    assert np.isclose(water_filling.volume(heights, level), volume)
+
+
+@pytest.mark.parametrize(
+    "heights,target_volume,level",
+    [
+        ([1, 2, 3, 4], 0.0, 1.0),  # Non-unique solution
+        ([1, 2, 3, 4], 0.5, 1.5),
+        ([1, 2, 3, 4], 2.0, 2.5),
+        ([1, 2, 3, 4], 6.0, 4.0),
+        ([1, 2, 3, 4], 30.0, 10.0),
+        ([-1, 2, -3, -4], 0.0, -4.0),  # Non-unique solution
+        ([-1, 2, -3, -4], 0.5, -3.5),
+        ([-1, 2, -3, -4], 8.0, 0.0),
+        ([-1, 2, -3, -4], 12.5, 1.5),
+        ([-1, 2, -3, -4], 14.0, 2.0),
+        ([-1, 2, -3, -4], 18.0, 3.0),
+    ],
+)
+def test_level(heights, target_volume, level):
+    level_estimated = water_filling.level(heights, target_volume)
+    volume_achieved = water_filling.volume(heights, level_estimated)
+    assert np.isclose(volume_achieved, target_volume)
+    # looser tolerance here because the bisection search converges based on
+    # level, not volume, not level (which it doesn't know)
+    assert np.isclose(level_estimated, level, atol=1e-4)
---
+++ water_filling.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+
+def volume(heights, level):
+    """Volume of water above terrain with given heights.
+
+    For example, `terrain = [1, 4, 2, 3]` can be visualized as:
+
+        |
+        |   x
+        |   x   x
+        |   x x x
+        | x x x x
+         (0 1 2 3)
+
+    When `level = 2.5`, there is a volume of `1.5` units of water above the
+    terrain at index `0`, and `0.5` units of water at index `2`, for a total
+    volume of `2.0`.
+    """
+    heights = np.asarray(heights)
+    return np.clip(level - heights, 0, np.inf).sum()
+
+
+def level(heights, target_volume, max_iterations=5000):
+    """Determine the level such that `volume(heights, level) = target_volume.`
+
+    Uses the secant method.
+    """
+    if not target_volume >= 0.0:
+        raise ValueError(volume)
+
+    heights = np.asarray(heights)
+    lo = heights.min()
+    lo_volume = volume(heights, lo)
+    hi = heights.max() + target_volume / heights.size
+    hi_volume = volume(heights, hi)
+
+    for i in range(max_iterations):
+        slope = (target_volume - lo_volume) / (hi_volume - lo_volume)
+        mid = lo + (hi - lo) * slope
+        mid_volume = volume(heights, mid)
+        if np.isclose(mid_volume, target_volume):
+            return mid
+
+        if mid_volume > target_volume:
+            hi = mid
+        else:
+            lo = mid
+
+    raise RecursionError(i)
+
+
+if __name__ == "__main__":
+    print(level([1, 2, 3, 4], 0.4))
